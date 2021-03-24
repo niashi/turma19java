161b8cc8d62a58592009980ca259b13c4500e0b7
Niashi <mathias.pn7@gmail.com>
2021-03-24 16:54:31 -0300

    Adicionando a pasta Java e o projeto tiraZika

---
+++ Java/primeiros-projetos/src/tiraZika/OpaMundao.java
@@ -0,0 +1,13 @@
+package tiraZika;
+
+public class OpaMundao {
+		
+	public static void main (String[] args) {
+			
+		// Escreva
+		System.out.println("Opa, Mundão!\n");
+		System.out.println("Bora programar em Java!");
+			
+	}
+		
+}
