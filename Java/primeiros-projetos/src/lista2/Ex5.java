577bb64887765dc17c08b898e92e1042029f0dcc
Niashi <mathias.pn7@gmail.com>
2021-03-28 01:55:01 -0300

    Adicionando o restante dos exercícios da lista 2 de Java

--- Java/primeiros-projetos/src/lista2/Ex5.java
+++ Java/primeiros-projetos/src/lista2/Ex5.java
@@ -1,5 +1,52 @@
 package lista2;
 
-public class Ex5 {
+import java.util.Scanner;
 
+public class Ex5 {
+	
+	public static void main(String[] args) {
+		
+		// Sistema que lê o índice de poluição da Secretaria de Meio Ambiente e emite
+		// uma notificação adequada aos diferentes grupos de indústrias poluidoras (1, 2 e 3).
+		
+		
+		double indice = 0.01;
+		int grupo;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.print("Qual é o grupo de sua indústria? 1, 2 ou 3? ");
+		grupo = leia.nextInt();
+		
+		if (grupo < 1 || grupo > 3) {
+			System.out.println("\nPor favor, insira um grupo válido.");
+		} else {
+			System.out.print("\nE qual é o índice de poluição total? ");
+			indice = leia.nextDouble();
+			
+			if (indice < 0) {
+				System.out.println("\nPor favor, insira um valor válido para o índice.");
+			} else if (indice == 0) {
+				System.out.println("\nTem certeza que o índice está correto?");
+			} else if (indice > 0 && indice < 0.3) {
+				System.out.println("\nO índice está aceitável. Não é necessário suspender as atividades industriais.");
+			} else if (indice >= 0.3 && indice < 0.4) {
+				if (grupo == 1) {
+					System.out.println("\nO índice está elevado. As indústrias do seu grupo terão suas atividades suspensas.");
+				} else {
+					System.out.println("\nO índice está elevado, mas as indústrias do seu grupo poderão seguir operando.");
+				}
+			} else if (indice >= 0.4 && indice < 0.5) {
+				if (grupo == 1 || grupo == 2) {
+					System.out.println("\nO índice está muito elevado. As indústrias dos grupos 1 e 2 terão suas atividades suspensas.");
+				} else {
+					System.out.println("\nO índice está muito elevado, mas as indústrias do seu grupo poderão seguir operando.");
+				}
+			} else {
+				System.out.println("\nO índice está extremamente elevado. As indústrias dos grupos 1, 2 e 3 terão suas atividades " + 
+				"suspensas.");
+			}
+		}
+	}
+	
 }
--- Java/primeiros-projetos/src/lista2/Ex8.java
+++ Java/primeiros-projetos/src/lista2/Ex8.java
@@ -1,5 +1,25 @@
 package lista2;
 
-public class Ex8 {
+import java.util.Scanner;
 
+public class Ex8 {
+	
+	public static void main(String[] args) {
+		
+		// Sistema que lê uma variável numérica N e a imprime caso ela seja maior que 100, e, caso não seja, imprime o valor 0.
+		
+		
+		double N;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.print("Forneça um número: ");
+		N = leia.nextDouble();
+		
+		if (N > 100) {
+			System.out.println("\nO número fornecido foi " + N + ".\n");
+		} else {
+			System.out.println("\nO número fornecido foi zero (eu sei que não foi, mas o sistema insere esse valor forçadamente, rs.\n");
+		}
+	}
 }
