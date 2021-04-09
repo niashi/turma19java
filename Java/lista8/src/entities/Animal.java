02ad055e02627f640f87fcd2c87f15a604b47818
Niashi <mathias.pn7@gmail.com>
2021-04-08 23:54:19 -0300

    Adicionando lista 8 de Java e seus exercícios

---
+++ Java/lista8/src/entities/Animal.java
@@ -0,0 +1,45 @@
+package entities;
+
+public class Animal {
+
+	// Atributos
+	private String nome;
+	private int idade;
+	
+	//Construtor
+	public Animal(String nome, int idade) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+	}
+	
+	// Getters & Setters
+	public String getNome() {
+		return nome;
+	}
+
+	private void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	private void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	// Métodos
+	public void emitirSom(String som) {
+		if (som == "au") {
+			System.out.println("au");
+		} else if (som == "hiin in in hinir") {
+			System.out.println("hiin in in hinir");
+		}			
+	}
+	
+	public void correr() {
+		System.out.println("O animal saiu correndo...");
+	}
+}
